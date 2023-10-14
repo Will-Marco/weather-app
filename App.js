8432d2f4d54fa05d9e46f58891e31fab5b8c1af2
Will-Marco <mr.marco0101@gmail.com>
2023-10-14 14:14:26 +0500

    add getLocation

--- App.js
+++ App.js
@@ -1,5 +1,39 @@
-import Loader from "./components/Loader";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import { Loader, Weather } from "./components";
 
 export default function App() {
-  return <Loader />;
+  const [isLoading, setIsLoading] = useState(true);
+  const { location, setLocation } = useState({
+    latitude: null,
+    longitude: null,
+  });
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestBackgroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permisson to access locationd was denied");
+        return;
+      }
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
+      setLocation({ latitude, longitude });
+    } catch (error) {
+      Alert.alert("I can't find your current location, so bad :(");
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+      getLocation();
+      console.log(location);
+    }, 1000);
+  }, []);
+
+  return isLoading ? <Loader /> : <Weather />;
 }
